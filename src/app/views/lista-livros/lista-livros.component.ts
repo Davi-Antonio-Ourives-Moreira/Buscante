302053d33ec0b5086e127787dab37bcfc1b039f8
Davi-Antonio-Ourives-Moreira <daviaomoreira@gmail.com>
2025-01-25 10:12:44 -0300

    otimizacao pesquisa

--- src/app/views/lista-livros/lista-livros.component.ts
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -1,51 +1,57 @@
 import { Item } from './../../models/interfaces/interface';
-import { FormsModule } from '@angular/forms';
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Component, DoCheck, OnDestroy, OnInit } from '@angular/core';
 import { LivroComponent } from '../../components/livro/livro.component';
 import { CommonModule } from '@angular/common';
 import { LivroService } from '../../services/livro.service';
-import { Subscription } from 'rxjs';
+import { catchError, debounceTime, filter, map, of, Subscription, switchMap, throwError } from 'rxjs';
 import { Livro } from '../../models/interfaces/interface';
 import { LivroVolumeInfo } from '../../models/interfaces/livroVolumeInfo';
+import { log } from 'node:console';
+
+const PAUSA = 300;
 
 @Component({
   selector: 'app-lista-livros',
   standalone: true,
-  imports: [LivroComponent, CommonModule, FormsModule],
+  imports: [LivroComponent, CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './lista-livros.component.html',
   styleUrl: './lista-livros.component.css'
 })
-export class ListaLivrosComponent implements OnDestroy {
-  valorInput: string = '';
-
-  listaLivros: Livro[] = [];
-
-  subscription!: Subscription;
-
-  livro!: Livro;
+export class ListaLivrosComponent {
+  valorInput = new FormControl;
 
   constructor(private service: LivroService){}
 
-  pesquisar(){
-    this.subscription = this.service.pesquisarLivros(this.valorInput).subscribe({
-      next: (items) => {
-        this.listaLivros = this.todosLivrosPesquisados(items);
-      },
-      error: error => console.log(error),
-      complete: () => console.log("Observable completo!")
-    });
-  }
+
+
+  livrosEncontrados$ = this.valorInput.valueChanges.pipe(
+    debounceTime(
+      PAUSA
+    ),
+    filter(
+      (valorDigitado) => valorDigitado.length >= 3
+      ),
+    switchMap((valor) => {
+      if (!valor){
+        return of([])
+      }
+
+      return this.service.pesquisarLivros(valor);
+    }),
+    map(
+      items => this.todosLivrosPesquisados(items)
+    ),
+    catchError(erro => {
+      console.log(erro);
+      return throwError(() => new Error('Aconteceu um erro inesperado!'))
+    })
+  )
+
 
   todosLivrosPesquisados(items: Item[]): Livro[] {
     return items.map(item => {
       return new LivroVolumeInfo(item);
     })
   }
-
-  ngOnDestroy(): void {
-    if (this.subscription) {
-      this.subscription.unsubscribe();
-    }
-  }
-
 }
